022ab016b2f3ff421d6b00266b9af610c0a6b83e
PatriciaCeausene <Oana.Ceausene@softwire.com>
2023-06-27 12:34:31 +0300

    Completed FizzBuzz task

---
+++ index.ts
@@ -0,0 +1,106 @@
+// This is our main function
+import * as readline from 'readline';
+
+function fizzbuzz(upperLimit:number, flag3:boolean, flag5:boolean, flag7:boolean, flag11:boolean, flag13:boolean, flag17:boolean): void {
+    console.log("Hello, World!");
+    // Put your code here...
+    for(let i = 1; i <= upperLimit; i++) {
+        let message:string = "";
+        let multiple11:boolean = false;
+        let reverse:boolean = false;
+        if(flag17 && i % 17 === 0) {
+            reverse = true;
+        }
+        if(flag11 && i % 11 === 0) {
+            if(flag13 && i % 13 == 0) {
+                if(reverse === true) {
+                    message = "Fezz" + message;
+                } else {
+                    message += "Fezz";
+                }
+            }
+            if(reverse === true) {
+                message = "Bong" + message;
+            } else {
+                message += "Bong";
+            }
+            multiple11 = true;
+        }
+        if(multiple11 === false) {
+            if (flag3 && i % 3 === 0) {
+                if(reverse === true) {
+                    message = "Fizz" + message;
+                } else {
+                    message += "Fizz";
+                }
+            }
+            if(flag13 && i % 13 === 0) {
+                if(reverse === true) {
+                    message = "Fezz" + message;
+                } else {
+                    message += "Fezz";
+                }
+            }
+            if (flag5 && i % 5 === 0) {
+                if(reverse === true) {
+                    message= "Buzz" + message;
+                } else {
+                    message += "Buzz";
+                }
+            }
+            if (flag7 && i % 7 === 0) {
+                if(reverse === true) {
+                    message = "Bang" + message;
+                } else {
+                    message += "Bang";
+                }
+            }
+            if (message === "") {
+                message += i;
+            }
+        }
+        console.log(message);
+    }
+}
+
+// Now, we run the main function:
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Insert a number ', (answer) => {
+    let args = answer.split(" ");
+    let myRecord:Record<number,boolean> = {};
+    let limit = parseInt(args[0]);
+    let flag3 = false;
+    let flag5 = false;
+    let flag7 = false;
+    let flag11 = false;
+    let flag13 = false;
+    let flag17 = false;
+    for (let i = 1; i < args.length; i++) {
+        let ruleNo:number = parseInt(args[i]);
+        if(ruleNo == 3) {
+            flag3 = true;
+        }
+        if(ruleNo == 5) {
+            flag5 = true;
+        }
+        if(ruleNo == 7) {
+            flag7 = true;
+        }
+        if(ruleNo == 11) {
+            flag11 = true;
+        }
+        if(ruleNo == 13) {
+            flag13 = true;
+        }
+        if(ruleNo == 17) {
+            flag17 = true;
+        }
+    }
+    fizzbuzz(limit, flag3, flag5, flag7, flag11, flag13, flag17);
+    rl.close();
+});
+//fizzbuzz();
